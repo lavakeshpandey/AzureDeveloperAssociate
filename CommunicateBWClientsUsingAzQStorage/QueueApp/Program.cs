e3b67a3b403a3b076a3fdc5413d029505faed2fb
Lavakesh Pandey <lavakeshpandey@outlook.com>
2021-03-09 12:53:30 +0530

    added comment

--- CommunicateBWClientsUsingAzQStorage/QueueApp/Program.cs
+++ CommunicateBWClientsUsingAzQStorage/QueueApp/Program.cs
@@ -16,7 +16,7 @@ namespace QueueApp
         {
             string value = Console.ReadLine();
             SendArticleAsync(value).Wait();
-            Console.WriteLine($"Sent messages: {value}");
+            Console.WriteLine($"Sent message...: {value}");
 
             Console.WriteLine($"Retrieving messages....");
 
